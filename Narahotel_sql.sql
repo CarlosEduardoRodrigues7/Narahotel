e279ec23d7c14bc0a83fbc115578a5fe545b3cbd
Felipe-0310 <ofelipe0310@gmail.com>
2026-09-18 17:07:58 -0300

    Add files via upload

--- Narahotel_sql.sql
+++ Narahotel_sql.sql
@@ -15,8 +15,6 @@ estado_origem VARCHAR(100) NOT NULL,
 faixa_etaria VARCHAR(150) NOT NULL,
 tipo_cliente VARCHAR(100) NOT NULL);
 
-DROP TABLE clientes;
-
 
 CREATE TABLE unidades (
 id_unidade INT PRIMARY KEY,
@@ -46,65 +44,66 @@ capacidade_max INT,
 valor_diaria_base DECIMAL (10,2));
 
 CREATE TABLE reservas (
-id_reserva INT PRIMARY KEY,
-id_unidade INT NOT NULL,
-id_tipo_quarto INT NOT NULL,
-id_cliente INT NOT NULL,
-id_canal INT NOT NULL,
-id_funcionario INT NOT NULL,
-data_checkin DATE NOT NULL,
-data_checkout DATE NOT NULL,
-qtd_diarias INT NOT NULL,
-num_hospedes INT NOT NULL,
-avaliacao_hospede DECIMAL(10,2),
-status_reserva VARCHAR(100),
-forma_pagamento VARCHAR(100),
-FOREIGN KEY (id_unidade)
-REFERENCES unidades (id_unidade),
-FOREIGN KEY (id_tipo_quarto)
-REFERENCES tipos_quarto (id_tipo_quarto),
-FOREIGN KEY (id_cliente)
-REFERENCES clientes (id_cliente),
-FOREIGN KEY (id_canal)
-REFERENCES canais_de_venda(id_canal),
-FOREIGN KEY (id_funcionario)
-REFERENCES funcionarios (id_funcionario));
+    id_reserva INT PRIMARY KEY,
+    id_unidade INT NOT NULL,
+    id_tipo_quarto INT NOT NULL,
+    id_cliente INT NOT NULL,
+    id_canal INT NOT NULL,
+    id_funcionario INT NOT NULL,
+    data_checkin DATE NOT NULL,
+    data_checkout DATE NOT NULL,
+    qtd_diarias INT NOT NULL,
+    num_hospedes INT NOT NULL,
+    avaliacao_hospede DECIMAL(10,2),
+    status_reserva VARCHAR(100),
+    forma_pagamento VARCHAR(100),
+    FOREIGN KEY (id_unidade)
+        REFERENCES unidades (id_unidade),
+    FOREIGN KEY (id_tipo_quarto)
+        REFERENCES tipos_quarto (id_tipo_quarto),
+    FOREIGN KEY (id_cliente)
+        REFERENCES clientes (id_cliente),
+    FOREIGN KEY (id_canal)
+        REFERENCES canais_de_venda(id_canal)
+);
 
+DROP TABLE reservas;
 
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
 
 SET GLOBAL local_infile = 1;
 
 
-LOAD DATA INFILE "C:/Users/felipe.espirito.SENACRJEDU/Downloads/canais_venda (1).csv"
+LOAD DATA LOCAL INFILE "C:/Users/PC GAMER/Downloads/canais_venda.csv"
 INTO TABLE canais_de_venda
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id_canal,nome_canal,comissao_pct);
 
 
-LOAD DATA INFILE "C:/Users/felipe.espirito.SENACRJEDU/Downloads/clientes.concat.csv"
+LOAD DATA LOCAL INFILE "C:/Users/PC GAMER/Downloads/clientes.concat.csv"
 INTO TABLE clientes 
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id_cliente,nome,cidade_origem,estado_origem,faixa_etaria,tipo_cliente);
 
 
 
-LOAD DATA INFILE "C:/Users/felipe.espirito.SENACRJEDU/Downloads/funcionarios.concat.csv"
+LOAD DATA LOCAL INFILE "C:/Users/PC GAMER/Downloads/funcionarios.concat.csv"
 INTO TABLE funcionarios
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id_funcionario,id_unidade,nome,cargo,departamento,salario,data_admissao);
 
+SELECT COUNT(*) AS total_funcionarios
+FROM funcionarios;
 
-
-
-LOAD DATA INFILE "C:/Users/felipe.espirito.SENACRJEDU/Downloads/unidades.concat.csv"
+LOAD DATA LOCAL INFILE "C:/Users/PC GAMER/Downloads/unidades.concat.csv"
 INTO TABLE unidades
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id_unidade,nome_unidade,cidade,regiao,categoria_hotel,num_quartos_total);
@@ -112,52 +111,50 @@ IGNORE 1 ROWS
 
 
 
-LOAD DATA INFILE "C:/Users/felipe.espirito.SENACRJEDU/Downloads/tipos_quarto.concat.csv"
+LOAD DATA LOCAL INFILE "C:/Users/PC GAMER/Downloads/tipos_quarto.concat.csv"
 INTO TABLE tipos_quarto
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id_tipo_quarto,descricao,capacidade_max,valor_diaria_base);
 
 
-LOAD DATA INFILE "C:/Users/felipe.espirito.SENACRJEDU/Downloads/reservas.concat (1).csv"
+LOAD DATA LOCAL INFILE "C:/Users/PC GAMER/Downloads/reservas.concat.corrigido.csv"
 INTO TABLE reservas
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id_reserva,id_unidade,id_tipo_quarto,id_cliente,id_canal,data_checkin,data_checkout,qtd_diarias,num_hospedes,avaliacao_hospede,status_reserva,forma_pagamento);
 
-SELECT * FROM reservas
-WHERE valor > 100;
+
+SELECT COUNT(*) AS total_reservas
+FROM reservas;
+
+SELECT COUNT(DISTINCT id_reserva) AS reservas_unicas
+FROM reservas;
+
+SELECT * FROM reservas;
+
+SELECT COUNT(*) AS total,
+       COUNT(DISTINCT id_reserva) AS ids_unicos
+FROM reservas;
 
 
-SELECT id_produto, valor FROM vendas
-WHERE id_loja = 3;
+SELECT 'canais_de_venda' AS tabela, COUNT(*) AS quantidade FROM canais_de_venda
+UNION ALL
+SELECT 'clientes', COUNT(*) FROM clientes
+UNION ALL
+SELECT 'unidades', COUNT(*) FROM unidades
+UNION ALL
+SELECT 'funcionarios', COUNT(*) FROM funcionarios
+UNION ALL
+SELECT 'tipos_quarto', COUNT(*) FROM tipos_quarto
+UNION ALL
+SELECT 'reservas', COUNT(*) FROM reservas;
 
 
-SELECT id_produto, valor, data_venda FROM vendas
-WHERE data_venda >= '2024-03-01'
-AND data_venda <= '2024-03-31';
-
-SELECT id_produto, valor FROM vendas
-WHERE id_produto IN (5, 12, 47);
 
 
-SELECT id_produto, SUM(valor) AS total_vendas
-FROM vendas
-GROUP BY id_produto
-ORDER BY total_vendas DESC;
 
 
-SELECT id_produto, SUM(valor) AS total_vendas
-FROM vendas
-WHERE data_venda >= "2024-06-01"
-GROUP BY id_produto
-HAVING total_vendas >= 10000
-ORDER BY total_vendas DESC;
-
-
-SELECT id_loja, COUNT(id_venda) AS quantidade
-FROM vendas
-GROUP BY id_loja;
 
